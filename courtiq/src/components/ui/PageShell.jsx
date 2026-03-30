8310e9bf1b6a9c90b5930c15949938114c68306e
PivotTraining <135228711+PivotTraining@users.noreply.github.com>
2026-03-30 14:20:37 -0400

    Delete courtiq directory

--- courtiq/src/components/ui/PageShell.jsx
+++
@@ -1,7 +0,0 @@
-export default function PageShell({ children, className = '' }) {
-  return (
-    <div className={`mx-auto w-full max-w-md px-8 py-10 sm:px-8 sm:py-10 ${className}`}>
-      {children}
-    </div>
-  )
-}
